675a5d3ac69d29d7a35bdbd0e1c4e39ccca2870f
iamtariqueanjum <tariqueanjummd9302@gmail.com>
2026-02-08 08:50:58 +0530

    LangGraph - compile and invoke stages

--- langgraph_learn/chat.py
+++ langgraph_learn/chat.py
@@ -5,14 +5,16 @@ from langgraph.graph.message import add_messages
 
 
 class State(TypedDict):
-    messages = Annotated[list, add_messages]
+    messages: Annotated[list, add_messages]
 
 
 def chatbot(state: State):
+    print("\nInside chatbot node", state)
     return {"messages": ["hey there! This is a message from chatbot!"]}
 
 
 def samplenode(state: State):
+    print("\nInside samplenode node", state)
     return {"messages": ["Sample Node message appended."]}
 
 
@@ -23,4 +25,13 @@ graph_builder.add_node("chatbot", chatbot)
 graph_builder.add_node("samplenode", samplenode)
 # state : {"messages": ["Hello World!", "hey there! This is a message from chatbot!", "Sample Node message appended."]}
 
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", "samplenode")
+graph_builder.add_edge("samplenode", END)
+# (START) -> chatbot -> samplenode -> (END)
 
+graph = graph_builder.compile()
+
+updated_state = graph.invoke(State({"messages": ["Hi, My name is Tarique"]}))
+
+print("\nupdated_state", updated_state)
