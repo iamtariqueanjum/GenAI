5df3fd24bce43411509a56a1a50d0d4e2b68c3c1
iamtariqueanjum <tariqueanjummd9302@gmail.com>
2026-02-08 19:08:39 +0530

    Knowledge Graphs using Neo4j

--- mem_agent/mem.py
+++ mem_agent/mem.py
@@ -12,6 +12,11 @@ client = OpenAI()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
+NEO4J_CONNECTION_URL = os.getenv("NEO4J_CONNECTION_URL")
+NEO4J_USERNAME = os.getenv("NEO4J_USERNAME")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+
+
 config = {
     "version": "v1.1",
     "embedder": {
@@ -21,6 +26,14 @@ config = {
     "llm": {
         "provider": "openai",
         "config": {"api_key": OPENAI_API_KEY, "model": "gpt-4.1"}
+    },
+    "graph_store": {
+        "provider": "neo4j",
+        "config": {
+            "url": NEO4J_CONNECTION_URL,
+            "username": NEO4J_USERNAME,
+            "password": NEO4J_PASSWORD
+        }
     },
     "vector_store": {
         "provider": "qdrant",
